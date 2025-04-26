65dd063e5f6e5c42c97ad3ed324cd4d5c43f9c69
abdullxhsaif <abdullahsaifpriv@gmail.com>
2025-04-26 16:25:25 +0500

    grade calculator

---
+++ grade.calc.js
@@ -0,0 +1,17 @@
+// Grade calculator
+
+var marks = +prompt("write your marks?");
+
+if (marks >= 90) {
+    console.log("grade A");
+} else if (marks >= 80) {
+    console.log("grade B");
+} else if (marks >= 70) {
+    console.log("grade c");
+} else if (marks >= 60) {
+    console.log("grade d");
+} else if (marks >= 50) {
+    console.log("grade e");
+} else {
+    console.log("Fail");
+}
